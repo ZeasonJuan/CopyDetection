3e581eca4053de39ba5e971067bedc5d41f31049
Yu Jianlong <2063217709@qq.com>
2022-11-17 14:45:31 +0800

    Reader class build findMethod method

--- Reader.py
+++ Reader.py
@@ -8,19 +8,24 @@ class Reader:
 
         self.file_list = self.haveFileList([])
         self.file_java_string = self.havaJavaFileString()
+        self.java_method = self.findMethod()
 
     def haveFileList(self, new_file_list):
+        """
+        :param new_file_list:
+        :return: list['filename']
+        """
         for home, dirs, files in os.walk(self.file_direct):
             for filename in files:
                 if filename.endswith(".java"):
                     new_file_list.append(op.join(home, filename))
 
-        return  new_file_list
-
-    def getFileList(self):
-        return self.file_list
+        return new_file_list
 
     def havaJavaFileString(self):
+        """
+        :return: Dict{'fileName':'context'}
+        """
         aDict = {}
         for i in range(len(self.file_list)):
             this_file = self.file_list[i]
@@ -28,11 +33,26 @@ class Reader:
                 aDict[this_file] = f.read()
         return aDict
 
+    def findMethod(self):
+        """
+        :return:Dict{"methodName":"context"}
+        """
+        methodDict = {}
+        for i in self.file_java_string.keys():
+            context = self.file_java_string.get(i)
+            """TODO"""
+        return methodDict
+
+    def getFileList(self):
+        return self.file_list
+
     def getJavaFileString(self):
         return self.file_java_string
+
+    def getJavaMethod(self):
+        return self.java_method
 
 
 if __name__ == "__main__":
     have_a_test = Reader("D:\java文件\idea\IdeaCode\RJQXAnalyzation")
-    file_string = have_a_test.getJavaFileString()
-    print(file_string.keys())
+    have_a_test.findClass()
