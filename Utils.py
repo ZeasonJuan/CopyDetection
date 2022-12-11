0a10ed6bcf17787c96c7e1764007bba098f4cceb
Yu Jianlong <2063217709@qq.com>
2022-12-11 19:26:47 +0800

    correct utils bug

--- Utils.py
+++ Utils.py
@@ -10,38 +10,43 @@ def generateControllerGraph(method):
         return {}
     # 将方法划分成行
     lines = method.split("\n")
-    for line in lines:
-        print(line)
+    # for line in lines:
+    #     print(line)
     # 对空行处理
     for i in range(len(lines)):
         if lines[i] == "":
             del lines[i]
-    for i in range(len(lines)):
+    nums = len(lines)
+    for i in range(nums):
         if lines[i].strip().split(" ")[0] in ["for", "while"]:
             k = 1
-            while i+step < len(lines):
+            while i+k < nums:
                 if getSpaceNumBefore(lines[i + k]) > getSpaceNumBefore(lines[i]):
                     k = k + 1
+                    if i + k == nums:
+                        lines.insert(i + k, " ")
                 else:
                     lines.insert(i+k, " ")
+                    break
     controllerGraph = {}
     controllerGraph[0] = [1]
     base_node = 0  # base_node,表示当前模块在起始模块开始的节点
     getGraph(lines, len(lines), base_node, controllerGraph, len(lines))
+    controllerGraph = sorted(controllerGraph.items(), key=lambda x:x[0])
     print(controllerGraph)
     return controllerGraph
 
 
 def getGraph(lines, node_num, base_node, controllerGraph, all_lines_num):
-    """
-    brief :动态生成图controllerGraph
-    param :lines 方法模块（if、for、while)对应的列表，元素为方法的一行
-           node_num lines的行数
-           base_node lines第一行在原始方法里的行号
-           controllerGraph 动态生成图
-           all_lines_num 总的行数
-    return：none
-    """
+    # """
+    # brief :动态生成图controllerGraph
+    # param :lines 方法模块（if、for、while)对应的列表，元素为方法的一行
+    #        node_num lines的行数
+    #        base_node lines第一行在原始方法里的行号
+    #        controllerGraph 动态生成图
+    #        all_lines_num 总的行数
+    # return：none
+    # """
     special_logic = ['if', 'while', 'for']
     count = 1  # 当前模块中的偏移地址,第一行已经处理过
     while count < node_num:
@@ -49,8 +54,7 @@ def getGraph(lines, node_num, base_node, controllerGraph, all_lines_num):
             # 生成子模块
             sub_lines, k = getSubLines(lines, node_num, count)
             # 逻辑处理
-            solveSpecialLogic(sub_lines, base_node + count, lines[count].strip().split(" ")[0], controllerGraph,
-                              all_lines_num)
+            solveSpecialLogic(sub_lines, base_node + count, lines[count].strip().split(" ")[0], controllerGraph,all_lines_num)
             count = count + k
         elif lines[count].strip().split(" ")[0] == "return":
             set = [all_lines_num]
@@ -64,13 +68,13 @@ def getGraph(lines, node_num, base_node, controllerGraph, all_lines_num):
 
 
 def getSubLines(lines, node_num, base_node):
-    """
-    brief:获取方法里的子模块(if-elif-else/for/while)
-    param :lines 方法模块对应的列表，元素为方法的一行
-           node_num lines的行数
-           base_node lines第一行在原始方法里的行号
-    return：子模块(if-elif-else/while/for)sub_lines
-    """
+    # """
+    # brief:获取方法里的子模块(if-elif-else/for/while)
+    # param :lines 方法模块对应的列表，元素为方法的一行
+    #        node_num lines的行数
+    #        base_node lines第一行在原始方法里的行号
+    # return：子模块(if-elif-else/while/for)sub_lines
+    # """
     sub_lines = [lines[base_node]]
     head_space_number = getSpaceNumBefore(lines[base_node])
     k = 1
@@ -84,28 +88,30 @@ def getSubLines(lines, node_num, base_node):
                 break
         else:  # if模块
             while base_node + k < node_num:
-                if getSpaceNumBefore(lines[base_node + k]) > head_space_number:
+                if getSpaceNumBefore(lines[base_node + k]) > head_space_number or lines[base_node+k] == " ":
                     sub_lines.append(lines[base_node + k])
                     k = k + 1
                 elif getSpaceNumBefore(lines[base_node + k]) == head_space_number:
-                    if lines[base_node + k].strip().split(" ")[0] in ["else", "elif"]:
+                    if lines[base_node + k].strip().split(" ")[0] in ["else", "elif", "else:"]:
                         sub_lines.append(lines[base_node + k])
                         k = k + 1
                     else:
                         break
+                else:
+                    break
             break
 
     return sub_lines, k
 
 
 def solveSpecialLogic(sub_lines, base_node, type, controllerGraph, all_lines_num):
-    """
-    paragm:sub_lines 进行处理的对象
-           base_node 子模块在原模块中的位置
-           len(sub_lines) 子模块的大小
-           type 子模块的类型
-           controllerGraph 处理结果，传址调用
-    """
+    # """
+    # paragm:sub_lines 进行处理的对象
+    #        base_node 子模块在原模块中的位置
+    #        len(sub_lines) 子模块的大小
+    #        type 子模块的类型
+    #        controllerGraph 处理结果，传址调用
+    # """
     special_logic = ['if', 'while', 'for']
     if type == special_logic[0]:
         print("solve if logic")
@@ -113,20 +119,12 @@ def solveSpecialLogic(sub_lines, base_node, type, controllerGraph, all_lines_num
         i = 1
         if_sub = []  # if-elif-else的标号
         # TODO deal with bug as followed
-        """
         while i < len(sub_lines):
-            if getSpaceNumBefore(sub_lines[i]) > head_space_num:
+            if getSpaceNumBefore(sub_lines[i]) > head_space_num or sub_lines[i] == " ":
                 i = i + 1
             else:
                 if_sub.append(i)
                 i = i + 1
-        """
-        while i < len(sub_lines):
-            if getSpaceNumBefore(sub_lines[i]) > head_space_num:
-                continue
-            else:
-                if_sub.append(i)
-            i = i + 1
         # 处理框架
         if if_sub is None:  # 只有if语句
             set = [base_node + 1]  # if指向下一句
@@ -139,7 +137,8 @@ def solveSpecialLogic(sub_lines, base_node, type, controllerGraph, all_lines_num
                 set = [base_node + start + 1]  # if指向下一句
                 set.append(base_node + k)
                 controllerGraph[base_node + start] = set
-                controllerGraph[base_node + k - 1] = [base_node + len(sub_lines)]
+                if base_node + k -1 not in controllerGraph.keys():
+                    controllerGraph[base_node + k - 1] = [base_node + len(sub_lines)]
                 getGraph(sub_lines[start:k], len(sub_lines[start:k]), base_node + start, controllerGraph, all_lines_num)
                 start = k
             # 对最后一个else/elif处理
@@ -162,12 +161,12 @@ def solveSpecialLogic(sub_lines, base_node, type, controllerGraph, all_lines_num
             if sub_lines[i].strip().split(" ")[0] == "break":
                 break_sub.append(i)
             i = i + 1
+        controllerGraph[base_node+len(sub_lines)-1] = [base_node]
         for continue_s in continue_sub:
             controllerGraph[base_node+continue_s] = [base_node]
         for break_s in break_sub:
             controllerGraph[base_node+break_s] = [base_node+len(sub_lines)]
-        controllerGraph[base_node+len(sub_lines)-1] = [base_node]
-        getGraph(sub_lines, len(sub_lines), base_node, controllerGraph, all_lines_num)
+        getGraph(sub_lines[:-1], len(sub_lines[:-1]), base_node, controllerGraph, all_lines_num)
     elif type == special_logic[2]:
         print("solve for logic")
         set = [base_node + 1]  # 指向for的下一句
@@ -182,12 +181,12 @@ def solveSpecialLogic(sub_lines, base_node, type, controllerGraph, all_lines_num
             if sub_lines[i].strip().split(" ")[0] == "break":
                 break_sub.append(i)
             i = i + 1
+        controllerGraph[base_node + len(sub_lines) - 1] = [base_node]
         for continue_s in continue_sub:
             controllerGraph[base_node + continue_s] = [base_node]
         for break_s in break_sub:
             controllerGraph[base_node + break_s] = [base_node + len(sub_lines)]
-        controllerGraph[base_node + len(sub_lines) - 1] = [base_node]
-        getGraph(sub_lines, len(sub_lines), base_node, controllerGraph, all_lines_num)
+        getGraph(sub_lines[:-1], len(sub_lines[:-1]), base_node, controllerGraph, all_lines_num)
 
 
 def getSpaceNumBefore(line):
